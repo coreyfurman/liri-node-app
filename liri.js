acbdabc40a5de48fa85340103ecead9f0c233074
coreyfurman <cfurm14@gmail.com>
2017-07-06 17:46:22 -0400

    updated

--- liri.js
+++ liri.js
@@ -1,44 +1,78 @@
-console.log("The bot is starting up");
+	var request = require("request");
+	var keys = require("./keys.js");
+	var twitter = require("twitter");
 
-var TW = require("twitter");
-
-const keys = require('./keys.js');
-console.log(k);
-var T = new Twitter(keys);
+var request = require('request');
+request('http://www.google.com', function (error, response, body) {
+  console.log('error:', error); /
+  console.log('statusCode:', response && response.statusCode); 
+  console.log('body:', body); 
+});
 
 switch (input) {
 
-	case "my-tweets":
-	twitterFunc();
-	break;
-
-	case "movie-this":
-	movieFunc( requestID );
-	break;
+	case "my-tweets": myTweets(); break;
+	case "movie-this": movieThis(); break;
 }
 
+function movieThis(){
+		var movie = process.argv[3];
+		if(!movie){
+			movie = "mr nobody";
+		}
+		params = movie
+		request("http://www.omdbapi.com/?t=" + params + "&y=&plot=short&r=json&tomatoes=true", function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				var movieObject = JSON.parse(body);
+				
+				var movieResults =
+				"------------------------------ begin ------------------------------" + "\r\n"
+				"Title: " + movieObject.Title+"\r\n"+
+				"Year: " + movieObject.Year+"\r\n"+
+				"Imdb Rating: " + movieObject.imdbRating+"\r\n"+
+				"Country: " + movieObject.Country+"\r\n"+
+				"Language: " + movieObject.Language+"\r\n"+
+				"Plot: " + movieObject.Plot+"\r\n"+
+				"Actors: " + movieObject.Actors+"\r\n"+
+				"Rotten Tomatoes Rating: " + movieObject.tomatoRating+"\r\n"+
+				"Rotten Tomatoes URL: " + movieObject.tomatoURL + "\r\n" + 
+				"------------------------------ fin ------------------------------" + "\r\n";
+				console.log(movieResults);
+				log(movieResults); 
+			} else {
+				console.log("Error :"+ error);
+				return;
+			}
+		});
+	};
 
-function twitterFunction() {
-
-const tKey = keys.twitterKeys;
-
-var T = new Twitter(keys);
-
-var params = {screen_name: '@forClass100'}; 
-
-client.get('statuses/user_timeline', { params, count: 20 }, function(error, tweets, response) {
-	for (var i = 0; i < tweets.length && i < 20; i++) {
-		console.log(tweets[i].text);
+	function myTweets() {
+		var client = new twitter({
+			consumer_key: keys.twitterKeys.consumer_key,
+			consumer_secret: keys.twitterKeys.consumer_secret,
+			access_token_key: keys.twitterKeys.access_token_key,
+			access_token_secret: keys.twitterKeys.access_token_secret, 
+		});
+		var twitterUsername = process.argv[3];
+		if(!twitterUsername){
+			twitterUsername = "forClass100";
+		}
+		params = {screen_name: twitterUsername};
+		client.get("statuses/user_timeline/", params, function(error, data, response){
+			if (!error) {
+				for(var i = 0; i < data.length; i++) {
+					
+					var twitterResults = 
+					"@" + data[i].user.screen_name + ": " + 
+					data[i].text + "\r\n" + 
+					data[i].created_at + "\r\n" + 
+					"------------------------------ " + i + " ------------------------------" + "\r\n";
+					console.log(twitterResults);
+					log(twitterResults); 
+				}
+			}  else {
+				console.log("Error :"+ error);
+				return;
+			}
+		});
 	}
-   
-   fs.appendFile("random.txt", tweets, function(err) {
-    				if (err) {
-      				return console.log(err);
-				    }
-				  });
-    if (error) {
-    	console.log("Error occured: " + error);
-    	return;
-    }
-
-});
